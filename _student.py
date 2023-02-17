45b84dcc50631e3a77201d961cf834fc983f3be8
ViniMig <vinicio.m14@gmail.com>
2023-02-17 03:31:01 +0000

    fix some bugs

--- _student.py
+++ _student.py
@@ -66,17 +66,11 @@ class Student:
                 day_dict["num_breaks"] += 1
                 day_dict["total_break_time"] += new_total
                 self.today_breaks += 1
-                if self.time_units == 'mins':# test if current total is in minutes
-                    self.today_total_time += int(day_dict["total_break_time"] / 60)
-                elif day_dict["total_break_time"] >= 60:# test if total time associated to student is in minutes
-                    self.today_total_time /= 60
-                    self.today_total_time += day_dict["total_break_time"]
+                if day_dict["total_break_time"] >= 60:# test if current total time associated to student is in minutes
+                    self.today_total_time = int(day_dict["total_break_time"] / 60)
                     self.time_units = 'mins'
-                else:# both are in seconds
-                    self.today_total_time += day_dict["total_break_time"]
-                    if self.today_total_time >= 60:# if the sum of both in seconds is more than or equal to 60
-                        self.today_total_time /= 60
-                        self.time_units = 'mins'
+                else: #the total is only in seconds so we only add the value. units will be in seconds already
+                    self.today_total_time = int(day_dict["total_break_time"])
     
     def get_breaks(self) -> tuple:
         """
--- _tk_gui.py
+++ _tk_gui.py
@@ -20,9 +20,15 @@ class ManagerGUI:
 
         #window configuration
         w, h = self.root.winfo_screenwidth(), self.root.winfo_screenheight()
-        self.root.geometry("%dx%d+0+0" % (w, h - 55))
+        self.root.geometry("%dx%d+0+0" % (w - 100, h - 100))
         self.root.title("Break time manager")
         self.root['background'] = '#856ff8'
+
+        # create a vertical scrollbar-no need
+        self.v_scroll = tk.Scrollbar(self.root, orient='vertical')
+        # attach Scrollbar to root window on
+        # the side
+        self.v_scroll.pack(side = 'right', fill = 'y')
 
         #add title label
         self.label = tk.Label(self.root, text = "Break Time!", background='#856ff8', fg='#ffffff', font = ('Comic Sans MS', 32, 'bold italic'))
@@ -47,6 +53,12 @@ class ManagerGUI:
         self.total_break_label = tk.Label(self.root, text = "Total break time today", background='#856ff8', fg='#ffffff', font = ('Comic Sans MS', 18, 'bold italic'))
         self.total_break_label.place(x = 900, y = 170, height = 50, width = 300)
 
+        self.info_frame = tk.Frame(self.root, bg = '#856ff8')
+        
+        self.info_canvas = tk.Canvas(self.info_frame, bg = '#856ff8', highlightthickness = 0, yscrollcommand = self.v_scroll.set)
+        self.info_frame.place(x = 0, y = 221, relheight = 1, relwidth = 0.991)
+        self.info_canvas.place(x = 0, y = 0, relheight = 1, relwidth = 1)
+
         #keep track of student buttons
         #use same cycle to create other elements
         self.st_btns = []
@@ -54,9 +66,9 @@ class ManagerGUI:
         self.br_labels = []
         self.br_time_labels = []
         self.current_break_time = [0 for _ in range(len(self.current_class.students))]
-        self.btn_x, self.btn_y, self.btn_w, self.btn_h = 50, 250, 250, 50
-        self.br_label_x, self.br_label_y, self.br_label_w, self.br_label_h = 500, 250, 250, 50
-        self.br_time_label_x, self.br_time_label_y, self.br_time_label_w, self.br_time_label_h = 900, 250, 250, 50
+        self.btn_x, self.btn_y, self.btn_w, self.btn_h = 50, 20, 250, 50
+        self.br_label_x, self.br_label_y, self.br_label_w, self.br_label_h = 500, 20, 250, 50
+        self.br_time_label_x, self.br_time_label_y, self.br_time_label_w, self.br_time_label_h = 900, 20, 250, 50
 
         for i, st in enumerate(self.current_class):
             #add student buttons
@@ -68,6 +80,10 @@ class ManagerGUI:
             #append total time label
             self.add_label("total_break_time", i)            
 
+        #configure scrolling action
+        #self.info_canvas.configure(height = 500, scrollregion=self.info_canvas.bbox("all"))
+        self.v_scroll.config(command=self.info_canvas.yview)
+
         #Configure application close
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
         self.root.mainloop()
@@ -77,7 +93,7 @@ class ManagerGUI:
         #append button state
         self.st_btns_state.append(True)
         #append button
-        self.st_btns.append(tk.Button(self.root, text=name, font = ('Arial', 15),
+        self.st_btns.append(tk.Button(self.info_canvas, text=name, font = ('Arial', 15),
             background='#2596be', fg='#ffffff', command= lambda c=iter_num: self.st_btn_click(c)))
         #place elements in interface
         self.st_btns[iter_num].place(x = self.btn_x, y = self.btn_y, height = self.btn_h, width = self.btn_w)
@@ -87,13 +103,13 @@ class ManagerGUI:
     def add_label(self, label_type: str, iter_num: int):
         """Add label to interface"""
         if label_type == "break_num_label":
-            self.br_labels.append(tk.Label(self.root, text = self.current_class.students[iter_num].today_breaks,
+            self.br_labels.append(tk.Label(self.info_canvas, text = self.current_class.students[iter_num].today_breaks,
                 background='#856ff8', fg='white', font = ('Comic Sans MS', 18, 'bold italic')))
             self.br_labels[iter_num].place(x = self.br_label_x, y = self.br_label_y, width = self.br_label_w, height = self.br_label_h)
             #increase y position for next element
             self.br_label_y += self.br_label_h + 10
         else:
-            self.br_time_labels.append(tk.Label(self.root, text = str(self.current_class.students[iter_num].today_total_time) + self.current_class.students[iter_num].time_units,
+            self.br_time_labels.append(tk.Label(self.info_canvas, text = str(self.current_class.students[iter_num].today_total_time) + self.current_class.students[iter_num].time_units,
                 background='#856ff8', fg='white', font = ('Comic Sans MS', 18, 'bold italic'))) 
             self.br_time_labels[iter_num].place(x = self.br_time_label_x, y = self.br_time_label_y, width = self.br_time_label_w, height = self.br_time_label_h)
             #increase y position for next element
@@ -101,7 +117,7 @@ class ManagerGUI:
     
     def new_student_window(self):
         """Action to add a new Student to the model and update the main interface"""
-        new_popup = Add_Student_GUI(self.root, self.current_class, self.add_student_button, self.add_label)
+        new_popup = Add_Student_GUI(self.info_canvas, self.current_class, self.add_student_button, self.add_label)
 
     def st_btn_click(self, btn_number: int):
         """Event for when clicking a student button change the state the color and act appropriately"""
